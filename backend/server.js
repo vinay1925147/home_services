71306db01d6e14c3a98a689d45ed1ab590136768
Vinay Asati <vinayasati90@gmail.com>
2026-05-02 12:09:19 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import adminRouter from "./routes/adminRoute.js"
 
 // app config
 const app = express()
-const port = process.env.PORT||4000
+const port = process.env.PORT|| 4000
 connectDB()
 connectCloudinary()
 
