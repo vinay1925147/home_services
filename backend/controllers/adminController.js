418235285d1cb5a7c11c0232e5ab90adcf267539
Vinay Asati <vinayasati90@gmail.com>
2026-05-02 13:05:05 +0530

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -104,8 +104,9 @@ const addDoctor = async (req, res) => {
 
         const newDoctor = new doctorModel(doctorData)
         await newDoctor.save()
-        res.json({ success: true, message: 'Doctor Added' })
+        res.json({ success: true, message: 'Services Added' })
 
+    
     } catch (error) {
         console.log(error)
         res.json({ success: false, message: error.message })
